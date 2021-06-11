30e6b14694e4e04bd9f0d7cb8f98f883714cbfc1
commitspammer <gustavo.ferreira.110@ufrn.edu.br>
2021-06-11 20:49:09 -0300

    Questao 'negativos5' finalizada.

--- negativos5/src/main.cpp
+++ negativos5/src/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+
 using std::cin;
 using std::cout;
 
@@ -6,7 +7,13 @@ const int SIZE = 5; // input size.
 
 int main(void)
 {
-    // TODO: Adicione aqui seu código.
-
-    return 0;
+	int num;
+	int count {0};
+	for (int i = 0; i < SIZE; i++) {
+		cin >> num;
+		if (num < 0)
+			count++;
+	}
+	cout << count;
+	return 0;
 }
