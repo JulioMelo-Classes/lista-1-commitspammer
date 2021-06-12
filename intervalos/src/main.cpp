e29950516b58fc9e9399e7887b05e941c6368ba7
commitspammer <gustavo.ferreira.110@ufrn.edu.br>
2021-06-12 16:43:37 -0300

    Questao 'intervalos' finalizada.

--- intervalos/src/main.cpp
+++ intervalos/src/main.cpp
@@ -5,17 +5,46 @@
  */
 
 #include <iostream>
-using std::cout;
-using std::cin;
-using std::endl;
 #include <iomanip>
-using std::setprecision;
+#include <vector>
 
-// Se desejar, crie funções aqui, antes do main().
+using namespace std;
+
+vector<int> get_input(void)
+{
+	vector<int> input;
+	int num;
+	while(cin >> ws >> num)
+		input.push_back(num);
+	return input;
+}
+
+vector<double> get_interval_ratio(vector<int> list)
+{
+	vector<double> ratios {0, 0, 0, 0, 0};
+	for (int i : list) {
+		if (i < 0 || i >= 100)
+			++ratios[4];
+		else if (i < 25)
+			++ratios[0];
+		else if (i < 50)
+			++ratios[1];
+		else if (i < 75)
+			++ratios[2];
+		else if (i < 100)
+			++ratios[3];
+
+	}
+	for (double& x : ratios)
+		x = x * 100 / list.size();
+	return ratios;
+}
 
 int main(void)
 {
-    // TODO: Adicione aqui a sua solução.
-
-    return 0;
+	vector<int> input = get_input();
+	vector<double> ratios = get_interval_ratio(input);
+	for (double i : ratios)
+		cout << setprecision(4) << i << endl;
+	return 0;
 }
