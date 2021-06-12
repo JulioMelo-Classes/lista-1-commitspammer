d1eb8caa77fce611011efa3b532d2fc0a9db650b
commitspammer <gustavo.ferreira.110@ufrn.edu.br>
2021-06-11 22:39:14 -0300

    Questao 'soma_vizinhos' finalizada.

--- soma_vizinhos/src/main.cpp
+++ soma_vizinhos/src/main.cpp
@@ -4,13 +4,24 @@
  * @data June, 6th 2021
  */
 #include <iostream>
+
 using std::cout;
 using std::cin;
 using std::endl;
 
-int main( void )
+int main(void)
 {
-    // TODO: Adicione seu código aqui. 
-
-    return 0;
+	int first, consec;
+	while (cin >> first >> consec) {
+		int sum {first};
+		if (consec > 0) {
+			for (int i = 1; i < consec; ++i)
+				sum += first + i;
+		} else if (consec < 0) {
+			for (int i = -1; i > consec; --i)
+				sum += first + i;
+		}
+		cout << sum << endl;
+	}
+	return 0;
 }
