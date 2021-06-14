abeb3e69f6ff076a0b8bf88519b5f9fd83342a54
commitspammer <gustavo.ferreira.110@ufrn.edu.br>
2021-06-14 13:04:50 -0300

    Questao 'ponto_em_retangulo_2' finalizada.

--- ponto_em_retangulo_2/src/function.cpp
+++ ponto_em_retangulo_2/src/function.cpp
@@ -1,11 +1,19 @@
 #include "function.h"
 
-/*! 
+/*!
  * Verifica se um ponto está dentro de um retângulo.
  */
-location_t pt_in_rect( const Ponto &IE, const Ponto &SD, const Ponto &P )
+location_t pt_in_rect(const Ponto &IE, const Ponto &SD, const Ponto &P)
 {
-    // TODO: Adicione seu código aqui.
-
-    return location_t::OUTSIDE;
+	if (IE.x == SD.x && IE.y == SD.y)
+		return location_t::INVALID;
+	if (P.x < IE.x || P.x > SD.x)
+		return location_t::OUTSIDE;
+	if (P.y < IE.y || P.y > SD.y)
+		return location_t::OUTSIDE;
+	if (P.x == IE.x || P.x == SD.x)
+		return location_t::BORDER;
+	if (P.y == IE.y || P.y == SD.y)
+		return location_t::BORDER;
+	return location_t::INSIDE;
 }
--- ponto_em_retangulo_2/src/main.cpp
+++ ponto_em_retangulo_2/src/main.cpp
@@ -8,20 +8,54 @@
 using std::cout;
 using std::cin;
 using std::endl;
+using std::ws;
 #include <algorithm>
-using std::min;
-using std::max;
+/*using std::min;
+using std::max;*/
+using std::swap;
+#include <vector>
+using std::vector;
 
 #include "function.h"
 
-//=== Funções
+vector<Ponto> get_coordinates()
+{
+	vector<Ponto> coords;
+	Ponto P;
+	int i {0};
+	while (i < 3 && cin >> ws >> P.x >> P.y) {
+		coords.push_back(P);
+		++i;
+	}
+	return coords;
+}
 
-// Coloque aqui qualquer função auxiliar que desejar.
-
+void fix_coordinates(vector<Ponto>& coords)
+{
+	if (coords[0].x > coords[1].x)
+		swap(coords[0].x, coords[1].x);
+	if (coords[0].y > coords[1].y)
+		swap(coords[0].y, coords[1].y);
+}
 
 int main(void)
 {
-    // TODO: Adicione aqui seu código.
-
-    return 0;
+	while (true) {
+		vector<Ponto> coords = get_coordinates();
+		if (coords.size() < 3)
+			break;
+		fix_coordinates(coords);
+		switch (pt_in_rect(coords[0], coords[1], coords[2])) {
+		case location_t::OUTSIDE:
+			cout << "outside" << endl; break;
+		case location_t::INSIDE:
+			cout << "inside" << endl; break;
+		case location_t::BORDER:
+			cout << "border" << endl; break;
+		case location_t::INVALID:
+			cout << "invalid" << endl; break;
+		}
+		coords.clear();
+	}
+	return 0;
 }
