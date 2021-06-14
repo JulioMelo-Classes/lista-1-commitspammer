5c544be0e5da04cbad2e0a5c84d19f8913c28e92
commitspammer <gustavo.ferreira.110@ufrn.edu.br>
2021-06-14 11:07:34 -0300

    Questao 'ponto_em_retangulo_1' finalizada.

--- ponto_em_retangulo_1/src/function.cpp
+++ ponto_em_retangulo_1/src/function.cpp
@@ -1,13 +1,19 @@
 #include "function.h"
 
-/*! 
+using namespace std;
+/*!
  * Verifica se um ponto está dentro de um retângulo.
  */
 
 location_t pt_in_rect( const Ponto &IE, const Ponto &SD, const Ponto &P )
 {
-    // TODO: Coloque aqui seu código.
-
-    // TODO: Substitua o retorno conforme desejar. Isso é apenas um STUB, usado apenas para compilar.
-    return location_t::OUTSIDE;
+	if (P.x < IE.x || P.x > SD.x)
+		return location_t::OUTSIDE;
+	if (P.y < IE.y || P.y > SD.y)
+		return location_t::OUTSIDE;
+	if (P.x == IE.x || P.x == SD.x)
+		return location_t::BORDER;
+	if (P.y == IE.y || P.y == SD.y)
+		return location_t::BORDER;
+	return location_t::INSIDE;
 }
